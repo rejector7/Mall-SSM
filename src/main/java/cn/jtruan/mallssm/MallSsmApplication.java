e0fdcf8391b8239b8353ddec1d86c5943491f871
rejector7 <2643719428@qq.com>
2021-06-24 15:55:42 +0800

    Initial commit

---
+++ src/main/java/cn/jtruan/mallssm/MallSsmApplication.java
@@ -0,0 +1,13 @@
+package cn.jtruan.mallssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/jtruan/mallssm/MallSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jtruan.mallssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
