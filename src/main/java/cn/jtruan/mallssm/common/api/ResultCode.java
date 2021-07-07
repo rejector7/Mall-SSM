b6c91eec61e355033a2b998dafed4ba3ff90f194
rejector7 <2643719428@qq.com>
2021-07-07 15:21:50 +0800

    [update] CRUD of brand and paging by pagehelper

--- src/main/java/cn/jtruan/mallssm/common/api/ResultCode.java
+++ src/main/java/cn/jtruan/mallssm/common/api/ResultCode.java
@@ -1,29 +1,27 @@
 package cn.jtruan.mallssm.common.api;
 
-/**
- * 枚举了一些常用API操作码
- * Created by macro on 2019/4/19.
- */
-public enum ResultCode implements IErrorCode {
+
+
+public enum ResultCode {
     SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
+    FAILED(500, "服务器内部操作失败"),
+    FORBIDDEN(403, "没有相关权限"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
+    VALIDATE_FAILED(404, "参数检验失败");
+
     private long code;
     private String message;
 
-    private ResultCode(long code, String message) {
+    private ResultCode(long code, String message){
         this.code = code;
         this.message = message;
     }
 
-    public long getCode() {
+    public long getCode(){
         return code;
     }
 
-    public String getMessage() {
+    public String getMessage(){
         return message;
     }
 }
-
