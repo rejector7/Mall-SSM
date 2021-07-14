b046da0b5fa345225da1e8747bbf9598ac916ed5
rejector7 <2643719428@qq.com>
2021-07-14 15:24:57 +0800

    [add] security config

--- src/main/java/cn/jtruan/mallssm/common/api/CommonResult.java
+++ src/main/java/cn/jtruan/mallssm/common/api/CommonResult.java
@@ -30,6 +30,20 @@ public class CommonResult<T> {
         return new CommonResult<>(ResultCode.FAILED.getCode(), ResultCode.FAILED.getMessage(), null);
     }
 
+    /**
+     * 未登录返回结果
+     */
+    public static <T> CommonResult<T> unauthorized(T data) {
+        return new CommonResult<T>(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMessage(), data);
+    }
+
+    /**
+     * 未授权返回结果
+     */
+    public static <T> CommonResult<T> forbidden(T data) {
+        return new CommonResult<T>(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMessage(), data);
+    }
+
     public long getCode() {
         return code;
     }
---
+++ src/main/java/cn/jtruan/mallssm/mbg/mapper/UmsRolePermissionRelationMapper.java
@@ -0,0 +1,30 @@
+package cn.jtruan.mallssm.mbg.mapper;
+
+import cn.jtruan.mallssm.mbg.model.UmsRolePermissionRelation;
+import cn.jtruan.mallssm.mbg.model.UmsRolePermissionRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRolePermissionRelationMapper {
+    long countByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRolePermissionRelation record);
+
+    int insertSelective(UmsRolePermissionRelation record);
+
+    List<UmsRolePermissionRelation> selectByExample(UmsRolePermissionRelationExample example);
+
+    UmsRolePermissionRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByExample(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRolePermissionRelation record);
+
+    int updateByPrimaryKey(UmsRolePermissionRelation record);
+}
