3e8b09ad8b8a4d45d3271d6784b1fb39cd0415ce
rejector7 <2643719428@qq.com>
2021-07-14 21:52:56 +0800

    [add] SpringTask

---
+++ src/main/java/cn/jtruan/mallssm/config/SpringTaskConfig.java
@@ -0,0 +1,11 @@
+package cn.jtruan.mallssm.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
